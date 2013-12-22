b9d1bb8b913b37ea183a69e357b952f94bfcfb51
Stefan Hendriks <stefanhen83@gmail.com>
2013-12-22 12:01:38 +0100

    introduce cell class with alive/dead state

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,10 @@
 describe "GameOfLife" do
-  describe "rules" do
+  describe "next generation" do
+    let(:cell) { Cell.new(alive) }
+
     describe "dead cell" do
-      subject { revives(neighbours) }
+      let(:alive) { false }
+      subject { cell.next_generation(neighbours) }
 
       context "revives with exactly 3 neighbours" do
         let(:neighbours) { 3 }
@@ -17,14 +20,11 @@ describe "GameOfLife" do
         let(:neighbours) { 2 }
         it { should be_false }
       end
-
-      def revives(neighbours)
-        neighbours == 3
-      end
     end
 
     describe "alive cell" do
-      subject { dies(neighbours) }
+      let(:alive) { true }
+      subject { cell.next_generation(neighbours) }
 
       context "with less then two neighbours" do
         let(:neighbours) { 1 }
@@ -45,10 +45,28 @@ describe "GameOfLife" do
         let(:neighbours) { 3 }
         it { should be_false }
       end
+    end
+  end
 
-      def dies(neighbours)
-        (neighbours < 2) || (neighbours > 3)
+  class Cell
+    def initialize(alive)
+      @alive = alive
+    end
+
+    def next_generation(neighbours)
+      if @alive
+        dies(neighbours)
+      else
+        revives(neighbours)
       end
+    end
+
+    def revives(neighbours)
+      neighbours == 3
+    end
+
+    def dies(neighbours)
+      (neighbours < 2) || (neighbours > 3)
     end
   end
 end
