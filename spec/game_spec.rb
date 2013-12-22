e543abb9c3d0d83a18e9329c5804b205a49253a9
Stefan Hendriks <stefanhen83@gmail.com>
2013-12-22 11:53:46 +0100

    introduce concept of dead cell

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,28 @@
 describe "GameOfLife" do
   describe "rules" do
+    describe "dead cell" do
+      subject { revives(neighbours) }
+
+      context "revives with exactly 3 neighbours" do
+        let(:neighbours) { 3 }
+        it { should be_true }
+      end
+
+      context "remains dead with more than 3 neighbours" do
+        let(:neighbours) { 4 }
+        it { should be_false }
+      end
+
+      context "remains dead with less than 2 neighbours" do
+        let(:neighbours) { 2 }
+        it { should be_false }
+      end
+
+      def revives(neighbours)
+        neighbours == 3
+      end
+    end
+
     describe "alive cell" do
       subject { dies(neighbours) }
 
@@ -22,7 +45,7 @@ describe "GameOfLife" do
         let(:neighbours) { 3 }
         it { should be_false }
       end
-z
+
       def dies(neighbours)
         (neighbours < 2) || (neighbours > 3)
       end
