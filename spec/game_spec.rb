1bb8ac8e92ecd6e23d82a1a4e55f1682d85e6f92
Stefan Hendriks <stefanhen83@gmail.com>
2014-04-10 19:37:03 +0200

    Use stateless Cell to calculate dead/alive state + add Generation

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,19 @@
 describe "GameOfLife" do
-  describe "next generation" do
-    let(:cell) { Cell.new(alive) }
+  describe "generation" do
+    let(:generation) { Generation.new(cells) }
+    subject { generation.next }
+
+    context "given an empty generation" do
+      let(:cells) { [] }
+      it { should eq([]) }
+    end
+  end
+
+  describe "Cell evolve" do
+    subject { Cell.evolve(alive, neighbours) }
 
     describe "dead cell" do
       let(:alive) { false }
-      subject { cell.next_generation(neighbours) }
 
       context "revives with exactly 3 neighbours" do
         let(:neighbours) { 3 }
@@ -24,49 +33,55 @@ describe "GameOfLife" do
 
     describe "alive cell" do
       let(:alive) { true }
-      subject { cell.next_generation(neighbours) }
 
-      context "with less then two neighbours" do
+      context "dies with less then two neighbours" do
         let(:neighbours) { 1 }
-        it { should be_true }
+        it { should be_false }
       end
 
-      context "with more than three neighbours" do
+      context "dies with more than three neighbours" do
         let(:neighbours) { 4 }
-        it { should be_true }
+        it { should be_false }
       end
 
       context "survives with exactly two neighbours" do
         let(:neighbours) { 2 }
-        it { should be_false }
+        it { should be_true }
       end
 
       context "survives with exactly three neighbours" do
         let(:neighbours) { 3 }
-        it { should be_false }
+        it { should be_true }
       end
     end
+
   end
 
   class Cell
-    def initialize(alive)
-      @alive = alive
-    end
-
-    def next_generation(neighbours)
-      if @alive
-        dies(neighbours)
+    def self.evolve(alive, neighbour_count)
+      if alive
+        !dies(neighbour_count)
       else
-        revives(neighbours)
+        revives(neighbour_count)
       end
     end
 
-    def revives(neighbours)
+    def self.revives(neighbours)
       neighbours == 3
     end
 
-    def dies(neighbours)
+    def self.dies(neighbours)
       (neighbours < 2) || (neighbours > 3)
+    end
+  end
+
+  class Generation
+    def initialize(cells)
+
+    end
+
+    def next
+      []
     end
   end
 end
